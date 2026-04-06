b006a02ee058b1ed184b14661e4e67add9ebca88
MaxHarker <maxharker02@gmail.com>
2026-04-05 23:43:53 -0600

    UI updates

--- src/components/SummaryCard.js
+++ src/components/SummaryCard.js
@@ -3,8 +3,10 @@ import React from "react";
 import "./SummaryCard.css";
 
 const SummaryCard = ({ title, count, color }) => {
+  const lightBackground = color + "20"
+
   return (
-    <div className="summary-card" style={{ borderLeft: `5px solid ${color}` }}>
+    <div className="summary-card" style={{ borderLeft: `5px solid ${color}`, backgroundColor: lightBackground }}>
       <h3 className="summary-title">{title}</h3>
       <p className="summary-count">{count}</p>
     </div>
